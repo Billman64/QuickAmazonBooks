ffc2fe07943735aa2cc2b9e6cccc65f3be248a8d
Bill <striderbillman@gmail.com>
2021-04-19 18:32:16 -0400

    Added author. UI tweaked for better visual layout in CardView. Animated recyclerView items upon entry.

--- app/src/main/java/com/github/billman64/quickamazonbooks/view/AmazonBookAdapter.kt
+++ app/src/main/java/com/github/billman64/quickamazonbooks/view/AmazonBookAdapter.kt
@@ -1,7 +1,10 @@
 package com.github.billman64.quickamazonbooks.view
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -12,8 +15,12 @@ class AmazonBookAdapter(private var responseItems: List<AmazonBooksResponseItem>
 
     inner class AmazonBookViewHolder(var binding: AmazonBookItemLayoutBinding): RecyclerView.ViewHolder(binding.root)
 
+    private lateinit var animation: Animation
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AmazonBookViewHolder {
         val binding = AmazonBookItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        animation = AnimationUtils.loadAnimation(parent.context, android.R.anim.slide_in_left)
         return AmazonBookViewHolder(binding)
 
     }
@@ -27,14 +34,24 @@ class AmazonBookAdapter(private var responseItems: List<AmazonBooksResponseItem>
 
         val book = responseItems[position]
 
+
         holder.binding.apply {
 
+            this.root.animation = animation
+
             Glide.with(holder.itemView)
-                .applyDefaultRequestOptions(RequestOptions.centerCropTransform())
+                .applyDefaultRequestOptions(RequestOptions.circleCropTransform())
                 .load(book.imageURL)
                 .into(bookCover)
 
             bookTitleTextview.text = book.title
+
+            book.author?.let{
+                authorTextview.text = it
+                authorTextview.visibility = View.VISIBLE
+            } ?: {
+                authorTextview.visibility = View.GONE
+            }()
         }
 
     }
